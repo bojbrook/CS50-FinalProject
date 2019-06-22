2ad9a0c9a42cc0f257da1b57954dd36cb3090879
Bowen Brooks <bowenjbrooks@gmail.com>
2019-06-22 08:42:48 -0700

    added platform and studio views plus started with some styling

--- game/urls.py
+++ game/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('game/<int:game_id>', views.game_view, name='game_view'),
 
     path('studios/', views.studios_view, name='studios_view'),
+    path('studios/<slug:studio_name>', views.studio_info_view, name='studio_info_view'),
 
     path('platforms/', views.platforms_view, name='platforms_view'),
+    path('platforms/<int:platform_id>', views.platform_info_view, name='platform_info_view')
 ]
--- game/views.py
+++ game/views.py
@@ -5,22 +5,12 @@ from .models import game, studio, Platform
 
 from datetime import timedelta, datetime, date
 
-def index(request):
-    try:
-        games = game.objects.all().order_by("release_date")
-    except game.DoesNotExist:
-        return HttpResponse("No Games")
-
+# returns all of the game data 
+def generate_game_data(games):
     game_info_arr = []
     for g in games:
-        # date = 
-        # t = timedelta(g.release_date)
         x = date.today()
         delta = abs((g.release_date - x))
-        # if(delta.days > )
-
-        # current = datetime(g.release_date.year,g.release_date.month,g.release_date.day)
-        # t = timedelta(current)
         print(f"Current Time {x} ")
         print(f"Delta Time {delta}")
         game_info = {
@@ -31,14 +21,22 @@ def index(request):
             "days_until":   abs((g.release_date - x).days)
         }
         game_info_arr.append(game_info)
+    return game_info_arr
 
-    print(game_info_arr)
+def index(request):
+    try:
+        games = game.objects.all().order_by("release_date")
+    except game.DoesNotExist:
+        return HttpResponse("No Games")
+
+    game_info_arr = generate_game_data(games)
     context = {
         'games': game_info_arr
     }
     return render(request,"game/index.html", context)
 
 
+# View for an individual game
 def game_view(request, game_id):
     try:
         title = game.objects.get(pk=game_id)
@@ -50,17 +48,44 @@ def game_view(request, game_id):
     }
     return render(request,"game/game_info.html", context)
 
+# A view of all of the studios
 def studios_view(request):
     try:
         studios = studio.objects.all()
     except studio.DoesNotExist:
         return HttpResponseRedirect(reverse("index"))
+    studios_arr = []
+    for Studio in studios:
+        info = {
+            "name":     Studio.name,
+            "url_name": Studio.name.replace(" ","_")
+        }
+        studios_arr.append(info)
 
+    print(studios_arr)
     context = {
-        "studios": studios
+        "studios": studios_arr
     }
     return render(request,"game/studios.html", context)
 
+# A view for each individual studio and all of their games
+def studio_info_view(request, studio_name):
+    studio_id = studio_name.replace("_"," ")
+    try:
+        Studio = studio.objects.get(pk=studio_id)
+        studio_games = game.objects.filter(studio=Studio).order_by("release_date")
+    except studio.DoesNotExist:
+        return HttpResponseRedirect(reverse("index"))
+    except game.DoesNotExist:
+        return HttpResponseRedirect(reverse("index"))
+
+    context = {
+        "studio":   Studio,
+        "games":    generate_game_data(studio_games)
+    }
+    return render(request,"game/studio_info.html",context)
+
+# A view for all of the platforms
 def platforms_view(request):
     try:
         platforms = Platform.objects.all()
@@ -71,3 +96,15 @@ def platforms_view(request):
         "platforms": platforms
     }
     return render(request,"game/platforms.html", context)
+
+def platform_info_view(request, platform_id):
+    try:
+        platform = Platform.objects.get(pk=platform_id)
+        platform_games = game.objects.filter(Platform=platform).order_by("release_date")
+    except studio.DoesNotExist:
+        return HttpResponseRedirect(reverse("index"))
+    context = {
+        "platform": platform,
+        "games":    generate_game_data(platform_games)
+    }
+    return render(request,"game/platform_info.html", context)
